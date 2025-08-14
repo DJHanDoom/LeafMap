60b8ac9718b9bcbdfcd30261b6f7b8306083c693
DJHanDoom <DJHANDOM@GMAIL.COM>
2025-08-14 19:39:53 -0300

    Update PhotoPicker.tsx

--- src/components/PhotoPicker.tsx
+++ src/components/PhotoPicker.tsx
@@ -7,20 +7,30 @@ export default function PhotoPicker({ onFirstPhoto, onMorePhotos }: Props) {
   return (
     <div className="card">
       <div>
-        <label>Foto inicial</label>
-        <input type="file" accept="image/*" capture="environment"
+        <label>Foto inicial (usa EXIF/GPS)</label>
+        <input
+          type="file"
+          accept="image/*"
+          capture="environment"
           onChange={e => {
             const f = e.target.files?.[0]
             if (f) onFirstPhoto(f)
-          }} />
+            if (e.target) (e.target as HTMLInputElement).value = ''
+          }}
+        />
       </div>
-      <div>
+      <div style={{ marginTop: 8 }}>
         <label>Fotos adicionais</label>
-        <input type="file" accept="image/*" multiple
+        <input
+          type="file"
+          accept="image/*"
+          multiple
           onChange={e => {
             const fs = Array.from(e.target.files ?? [])
             if (fs.length) onMorePhotos(fs)
-          }} />
+            if (e.target) (e.target as HTMLInputElement).value = ''
+          }}
+        />
       </div>
     </div>
   )
