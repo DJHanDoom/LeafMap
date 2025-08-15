d991e34a4c8a6979d70ac64b23105f35f365a230
DJHanDoom <DJHANDOM@GMAIL.COM>
2025-08-15 20:46:36 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: { host: true },
-  optimizeDeps: {
-    include: ['xlsx']        // garante pré-bundle no dev e ajuda o resolver
-  },
-  build: {
-    rollupOptions: {
-      // nada externo aqui; deixamos o Vite embutir o ESM
-    }
-  }
+  server: { host: true }
 })
