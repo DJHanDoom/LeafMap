da4a448d17c8284bba218f804e6bbad6a487ee98
DJHanDoom <DJHANDOM@GMAIL.COM>
2025-08-15 16:27:08 -0300

    Update PhotoPicker.tsx

--- src/components/PhotoPicker.tsx
+++ src/components/PhotoPicker.tsx
@@ -8,40 +8,37 @@ export default function PhotoPicker({ onCamera, onGallery }: Props) {
     <div className="card">
       <label>Fotos</label>
       <div className="row">
-        <div>
-          <button
-            onClick={() => {
-              const input = document.createElement('input')
-              input.type = 'file'
-              input.accept = 'image/*'
-              input.capture = 'environment'
-              input.onchange = e => {
-                const f = (e.target as HTMLInputElement).files?.[0]
-                if (f) onCamera(f)
-              }
-              input.click()
-            }}
-          >
-            Tirar foto (Câmera)
-          </button>
-        </div>
-        <div>
-          <button
-            onClick={() => {
-              const input = document.createElement('input')
-              input.type = 'file'
-              input.accept = 'image/*'
-              input.multiple = true
-              input.onchange = e => {
-                const fs = Array.from((e.target as HTMLInputElement).files ?? [])
-                if (fs.length) onGallery(fs)
-              }
-              input.click()
-            }}
-          >
-            Escolher da Galeria
-          </button>
-        </div>
+        <button
+          onClick={() => {
+            const input = document.createElement('input')
+            input.type = 'file'
+            input.accept = 'image/*'
+            input.capture = 'environment'
+            input.onchange = e => {
+              const f = (e.target as HTMLInputElement).files?.[0]
+              if (f) onCamera(f)
+            }
+            input.click()
+          }}
+        >
+          Tirar foto (Câmera)
+        </button>
+
+        <button
+          onClick={() => {
+            const input = document.createElement('input')
+            input.type = 'file'
+            input.accept = 'image/*'
+            input.multiple = true
+            input.onchange = e => {
+              const fs = Array.from((e.target as HTMLInputElement).files ?? [])
+              if (fs.length) onGallery(fs)
+            }
+            input.click()
+          }}
+        >
+          Escolher da Galeria
+        </button>
       </div>
       <small>Dica: use a câmera para capturar EXIF/GPS quando disponível.</small>
     </div>
