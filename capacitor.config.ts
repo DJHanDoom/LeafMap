ed90f6aafd16e437f05f6b77dfaa9218b4819f40
DJHanDoom <DJHANDOM@GMAIL.COM>
2025-08-15 22:09:12 -0300

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,9 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'br.com.diogo.trees',            // mantenho o mesmo appId (pacote). Se quiser mudar, me diga.
-  appName: 'NervuraColetora',             // <- nome que aparece no celular
-  webDir: 'dist',
+  appId: 'br.com.diogo.nervura',     // seu pacote Android (pode manter esse)
+  appName: 'NervuraColetora',        // nome exibido no celular
+  webDir: 'dist',                    // gerado pelo vite build
   bundledWebRuntime: false
 };
 
